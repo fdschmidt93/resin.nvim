d6eeb49dea6217c17bd24bf4b3cc911f59e44681
Fabian David Schmidt <fabian.david.schmidt@gmail.com>
2022-07-09 12:50:01 +0200

    chore(filetype.python): clean up

--- lua/resin/ft/python.lua
+++ lua/resin/ft/python.lua
@@ -1,9 +1,8 @@
 -- DISCLAIMER: only works with ipython
 return {
-  on_before_receive = function(receiver, data)
+  on_before_receive = function(receiver)
     receiver:receiver_fn { "%cpaste -q\n" } -- sending ipython header
     vim.wait(50) -- cpaste otherwise doesn't properly work
-    -- table.insert(data, "--\n")
   end,
   on_after_receive = function(receiver)
     receiver:receiver_fn { "\n--\n" } -- sending ipython header
