e0b6b55fead43607a69ab4fce2877eaab8494890
Fabian David Schmidt <fabian.david.schmidt@gmail.com>
2022-07-10 17:11:18 +0200

    chore(terminal): better setup & lazily access chan

--- lua/resin/receiver/neovim_terminal.lua
+++ lua/resin/receiver/neovim_terminal.lua
@@ -3,8 +3,17 @@ local Receiver = require "resin.receiver"
 return function(opts)
   opts = opts or {}
 
+  -- TODO notify more nicley with vim.notiy
+  assert(opts.bufnr, "Must pass valid bufnr!")
+  assert(vim.b[opts.bufnr].terminal_job_id, "Must be terminal buffer!")
+
   opts.receiver_fn = function(self, data)
-    vim.api.nvim_chan_send(self.chan, table.concat(data, "\n"))
+    -- access terminal_job_id lazily so restarting terminal in same buffer
+    vim.api.nvim_chan_send(vim.b[self.bufnr].terminal_job_id, table.concat(data, "\n"))
+  end
+
+  opts.exists = function(self)
+    return type(vim.b[self.bufnr].terminal_job_id) == "number"
   end
 
   return Receiver:new(opts)
