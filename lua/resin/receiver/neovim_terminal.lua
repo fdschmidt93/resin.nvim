071092a74a525fc79eda554d4da6bbee8bf28095
Fabian David Schmidt <fabian.david.schmidt@gmail.com>
2023-02-01 17:14:28 +0100

    fix(term): do not trim send data

--- lua/resin/receiver/neovim_terminal.lua
+++ lua/resin/receiver/neovim_terminal.lua
@@ -8,7 +8,7 @@ return function(opts)
   assert(vim.b[opts.bufnr].terminal_job_id, "Must be terminal buffer!")
 
   opts.receiver_fn = function(self, data)
-    local send_data = vim.trim(table.concat(data, "\n"))
+    local send_data = table.concat(data, "\n")
     vim.api.nvim_chan_send(
       vim.b[self.bufnr].terminal_job_id,
       string.format("%s%s%s", "\27[200~", send_data, "\27[201~\13\13")
